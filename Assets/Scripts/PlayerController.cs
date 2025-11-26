239c49c60ebf114febee04047d24095139366eab
Malte95 <maltescherenberg@gmx.de>
2025-11-26 12:06:11 +0100

    Rotate the Player GameObject to face the mouse, Apply thrust in mouse direction, Normalize the direction variable, Handle collisions

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,21 +1,48 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class Player : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float thrustForce = 1f;
+    Rigidbody2D rb;
+    public float maxSpeed = 5f;
+    public GameObject boosterFlame;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (Mouse.current.leftButton.isPressed)
         {
+            // Calculate mouse direction
             Vector3 mousePos = Camera.main.ScreenToWorldPoint(Mouse.current.position.value);
-            Debug.Log("Mouse position: " + mousePos);
+            Vector2 direction = (mousePos - transform.position).normalized;
+
+            // Move player in direction of mouse
+            transform.up = direction;
+            rb.AddForce(direction * thrustForce);
+
+            if (rb.linearVelocity.magnitude > maxSpeed)
+            {
+                rb.linearVelocity = rb.linearVelocity.normalized * maxSpeed;
+            }
         }
+
+        if (Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            boosterFlame.SetActive(true);
+        }
+        else if (Mouse.current.leftButton.wasReleasedThisFrame)
+        {
+            boosterFlame.SetActive(false);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(gameObject);
     }
 }
