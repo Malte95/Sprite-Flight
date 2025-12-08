71c7494d435c977dea60ae00c4af7d2902d2b392
Malte95 <maltescherenberg@gmx.de>
2025-12-08 15:02:43 +0100

    Style the label,Get a reference to the UI document, Set the score text with true score,Scale with screen size

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,54 +1,61 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
 
 public class PlayerController : MonoBehaviour
 {
-    public float thrustForce = 1f;
-    Rigidbody2D rb;
-    public float maxSpeed = 5f;
-    public GameObject boosterFlame;
     private float elapsedTime = 0f;
     private float score = 0f;
+
     public float scoreMultiplier = 10f;
+    public float thrustForce = 1f;
+
+    Rigidbody2D rb;
+
+    public UIDocument uiDocument;
+    private Label scoreText;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        scoreText = uiDocument.rootVisualElement.Q<Label>("ScoreLabel");
     }
 
     void Update()
     {
-        elapsedTime += Time.deltaTime;
-        score = Mathf.FloorToInt(elapsedTime * scoreMultiplier);
-        Debug.Log("Score: " + score);
-        if (Mouse.current.leftButton.isPressed) 
-        {
-            // Calculate mouse direction
-            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Mouse.current.position.value);
-            Vector2 direction = (mousePos - transform.position).normalized;
-
-            // Move player in direction of mouse
-            transform.up = direction;
-            rb.AddForce(direction * thrustForce);
-
-            if (rb.linearVelocity.magnitude > maxSpeed)
+        void UpdateScore()
             {
-                rb.linearVelocity = rb.linearVelocity.normalized * maxSpeed;
-            }
-        }
+            elapsedTime += Time.deltaTime;
+            score = Mathf.FloorToInt(elapsedTime * scoreMultiplier);
+            scoreText.text = "Score: " + score;
 
-        if (Mouse.current.leftButton.wasPressedThisFrame)
-        {
-            boosterFlame.SetActive(true);
-        }
-        else if (Mouse.current.leftButton.wasReleasedThisFrame)
-        {
-            boosterFlame.SetActive(false);
-        }
+            }
+        void MovePlayer()
+            {
+            if (Mouse.current.leftButton.isPressed)
+            {
+
+                // Calculate mouse direction
+                Vector3 mousePos = Camera.main.ScreenToWorldPoint(Mouse.current.position.value);
+                Vector2 direction = (mousePos - transform.position).normalized;
+
+                // Move player in direction of mouse
+                transform.up = direction;
+                rb.AddForce(direction * thrustForce);
+            }
+
+            }
+        UpdateScore();
+        MovePlayer();
+      
+
+
+
     }
 
     void OnCollisionEnter2D(Collision2D collision)
     {
         Destroy(gameObject);
     }
+
 }
